a0268374ad98a2f46b2559f67dffbbd59846d914
Paweł Rybak <pawel.rybak13@gmail.com>
2023-06-28 22:26:51 +0200

    Created subbytes and shiftrows function and their inverses

--- aes256/aes256.go
+++ aes256/aes256.go
@@ -63,3 +63,90 @@ func (a *AES256) NewExpKey() (*key.ExpandedKey, error) {
 
 	return xKey, nil
 }
+
+// SubBytes returns a state with every
+// byte replaced with it's corresponding
+// byte from the sbox.
+//
+// https://pl.wikipedia.org/wiki/Advanced_Encryption_Standard
+func (a *AES256) SubBytes(state []byte) ([]byte, error) {
+	if len(state) != consts.BLOCK_SIZE {
+		return nil, errors.New("state size not matching the block size")
+	}
+
+	var subState []byte
+
+	sbox := NewSBox()
+	for i := range state {
+		subState = append(subState, sbox[state[i]])
+	}
+
+	return subState, nil
+}
+
+// InvSubBytes undoes the SubBytes operation
+// allowing decryption.
+//
+// https://pl.wikipedia.org/wiki/Advanced_Encryption_Standard
+func (a *AES256) InvSubBytes(state []byte) ([]byte, error) {
+	if len(state) != consts.BLOCK_SIZE {
+		return nil, errors.New("state size not matching the block size")
+	}
+
+	var invSubState []byte
+
+	invsbox := NewInvSBox(NewSBox())
+	for i := range state {
+		invSubState = append(invSubState, invsbox[state[i]])
+	}
+
+	return invSubState, nil
+}
+
+// ShiftRows returns a state where the last three
+// rows has been transposed in an AES specific way.
+//
+// https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+func (a *AES256) ShiftRows(state []byte) ([]byte, error) {
+	if len(state) != consts.BLOCK_SIZE {
+		return nil, errors.New("state size not matching the block size")
+	}
+
+	var shiftedState []byte
+	copy(shiftedState, state)
+
+	for i := 1; i < 4; i++ {
+		j := i
+
+		shiftedState[i+(4*0)] = state[i+4*((j+0)%4)]
+		shiftedState[i+(4*1)] = state[i+4*((j+1)%4)]
+		shiftedState[i+(4*2)] = state[i+4*((j+2)%4)]
+		shiftedState[i+(4*3)] = state[i+4*((j+4)%4)]
+	}
+
+	return shiftedState, nil
+}
+
+// InvShiftRows undoes the ShiftRows operation
+// allowing decryption.
+//
+// https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
+func (a *AES256) InvShiftRows(state []byte) ([]byte, error) {
+	if len(state) != consts.BLOCK_SIZE {
+		return nil, errors.New("state size not matching the block size")
+	}
+
+	var invShiftedState []byte
+	copy(invShiftedState, state)
+
+	for i := 1; i < 4; i++ {
+		j := 4 - i
+
+		invShiftedState[i+(4*0)] = state[i+4*((j+0)%4)]
+		invShiftedState[i+(4*1)] = state[i+4*((j+1)%4)]
+		invShiftedState[i+(4*2)] = state[i+4*((j+2)%4)]
+		invShiftedState[i+(4*3)] = state[i+4*((j+4)%4)]
+	}
+
+	return invShiftedState, nil
+}
