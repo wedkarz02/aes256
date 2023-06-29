8e0f3eb9c61a72056489c3ecb9c944d23a81e1c0
Paweł Rybak <pawel.rybak13@gmail.com>
2023-06-29 14:02:59 +0200

    Removed reference comment

--- aes256/galois/galois.go
+++ aes256/galois/galois.go
@@ -8,25 +8,6 @@ func Gsub(a byte, b byte) byte {
 	return a ^ b
 }
 
-// private byte GMul(byte a, byte b) { // Galois Field (256) Multiplication of two Bytes
-//     byte p = 0;
-
-//     for (int counter = 0; counter < 8; counter++) {
-//         if ((b & 1) != 0) {
-//             p ^= a;
-//         }
-
-//         bool hi_bit_set = (a & 0x80) != 0;
-//         a <<= 1;
-//         if (hi_bit_set) {
-//             a ^= 0x1B; /* x^8 + x^4 + x^3 + x + 1 */
-//         }
-//         b >>= 1;
-//     }
-
-//     return p;
-// }
-
 func Gmul(a byte, b byte) byte {
 	var p byte = 0
 
