53ea3f336499fcc92cd78afa9285e1a8586c1e15
Paweł Rybak <pawel.rybak13@gmail.com>
2023-07-02 13:48:58 +0200

    Added zero padding functions

---
+++ src/padding/padding.go
@@ -0,0 +1,31 @@
+package padding
+
+import "github.com/wedkarz02/aes256go/src/consts"
+
+type Pad func([]byte) []byte
+type UnPad func([]byte) []byte
+
+func ZeroPadding(data []byte) []byte {
+	paddedData := make([]byte, len(data))
+	copy(paddedData, data)
+
+	remainder := len(paddedData) % consts.BLOCK_SIZE
+	padLength := consts.BLOCK_SIZE - remainder
+
+	for i := 0; i < padLength; i++ {
+		paddedData = append(paddedData, 0x00)
+	}
+
+	return paddedData
+}
+
+func ZeroUnpadding(paddedData []byte) []byte {
+	for paddedData[len(paddedData)-1] == 0x00 {
+		paddedData = paddedData[:len(paddedData)-1]
+	}
+
+	data := make([]byte, len(paddedData))
+	copy(data, paddedData)
+
+	return data
+}
